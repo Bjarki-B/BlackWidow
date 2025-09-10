c049f865a42c3572062cdc9f553b35cc7ead0546
rhgolshan <rhgolshan@ph1.uni-koeln.de>
2025-09-10 16:40:57 +0100

    changing from unitest to pytest

--- tests/test_scaling_relations_Curti2020.py
+++ tests/test_scaling_relations_Curti2020.py
@@ -1,25 +1,27 @@
-import unittest
+import pytest
 from scaling_relations_Curti2020 import line_ratios
 
 class TestLineRatios(unittest.TestCase):
     def test_Z_zero(self):
-        Z = 0
-        results = line_ratios(Z)
+        metallicity = 8.69
+        results = line_ratios(metallicity)
         
-        # 1. Check that the result is a dictionary
-        self.assertIsInstance(results, dict)
-        
-        # 2. Check that all expected diagnostics are present
-        expected_diagnostics = [
-            "R2", "R3", "O3O2", "R23", "N2", "O3N2", "S2", "RS32", "O3S2"
-        ]
-        for diag in expected_diagnostics:
-            self.assertIn(diag, results)
-        
-        # 3. Check that all values are floats
-        for val in results.values():
-            self.assertIsInstance(val, float)
+        results = line_ratios(metallicity)
 
-if __name__ == "__main__":
-    unittest.main()
+    # 1. Check that the result is a dictionary
+    assert isinstance(results, dict)
+
+    # 2. Check that all expected diagnostics are present
+    expected_diagnostics = [
+        "R2", "R3", "O3O2", "R23", "N2", "O3N2", "S2", "RS32", "O3S2"
+    ]
+    for diag in expected_diagnostics:
+        assert diag in results
+
+    # 3. Check that all values are floats
+    for val in results.values():
+        assert isinstance(val, float)
+
+
+
 
