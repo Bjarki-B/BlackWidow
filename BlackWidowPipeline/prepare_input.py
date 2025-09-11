c55fb32c1e2f73d55c87fe7f59a6bef2a8cc2bc8
Bjarki Bjorgvinsson <bjarki.bjorgvinsson98@gmail.com>
2025-09-11 15:56:00 +0100

    <feat> Changed handling of extensions of fits files The order of the extensions in no longer relevant as long as the EXTNAME in the header is correct. Also added handling of errors in dictionaries

--- BlackWidowPipeline/prepare_input.py
+++ BlackWidowPipeline/prepare_input.py
@@ -1,7 +1,7 @@
 import numpy as np
 from astropy.io import fits
 
-def flatten_input(path: str, ext_dict = None) -> dict:
+def flatten_input(path: str) -> dict:
     '''
     Open a FITS file, flatten its data, and return the flattened array. 
     path: str
@@ -14,14 +14,33 @@ def flatten_input(path: str, ext_dict = None) -> dict:
             Dictionary with emission lines as keys and flattened arrays as values.
     '''
     data_dict = {"Halpha":None, "Hbeta":None, "OII":None, "NII":None} # Dictionary for holding arrays
-    if  ext_dict is None:
-        ext_dict = {"Halpha":0, "Hbeta":1, "OII":2, "NII":3} # Defult extensions
-        
-    with fits.open(path) as hdul: # Open the FITS file
-        for line, ext in ext_dict.items(): # Loop over the emission lines
-            data_dict[line] = np.array((hdul[ext].data).flatten()) # Flatten and store in dictionary
+    err_dict = {"err_Halpha":None, "err_Hbeta":None, "err_OII":None, "err_OII":None} # Dictionary for holding error arrays
+    ext_dict = {}
+    lines = list(data_dict.keys())
+    errs = list(err_dict.keys())
+    try:
+        with fits.open(path) as hdul: # Open the FITS file
+            for ext, hdu in enumerate(hdul): # Loop over HDUList extensions
+                hdr = hdu.header
+                try:
+                    name = hdr['EXTNAME'] # Get the extension name from the header
+                    ext_dict[name] = ext
+                except KeyError:
+                    continue
+            for name, ext in ext_dict.items(): # Loop over found extensions
+                for line in lines:
+                    if name.lower() == line.lower():
+                        data_dict[line] = np.array((hdul[ext].data).flatten()) # Flatten and store in dictionary
+                        
+            for name, ext in ext_dict.items(): # Loop over found extensions
+                for err in errs:
+                    if name.lower() == err.lower():
+                        err_dict[err] = np.array((hdul[ext].data).flatten()) # Flatten and store in dictionary
+        return data_dict, err_dict
     
-    return data_dict
+    except FileNotFoundError:
+        raise FileNotFoundError(f"File not found: {path}")
+    
 
 def check_input(data = None):
     '''
@@ -29,6 +48,7 @@ def check_input(data = None):
     If it's a dictionary, validate its contents. If it's a path, process the FITS file with flatten_input.
     data: dict or str
         Either a dictionary with emission lines as keys and numpy arrays as values, or a path to a FITS file.
+        The emission lines in dictionary must be 'Halpha', 'Hbeta', 'OII', and 'NII' (case sensitive).
     returns:
         If input is a dictionary, returns the same dictionary after validation.
         If input is a FITS file path, returns a dictionary with flattened arrays.
@@ -41,3 +61,8 @@ def check_input(data = None):
         return flatten_input(data)
     else:
         raise ValueError("Input must be either a dictionary of numpy arrays or a path to a FITS file.")
+    
+    
+    
+if __name__ == "__main__":
+    final = check_input('~/Documents/astrodat/BlackWidow/example_manga_maps.fits')
