cc6204a49dc5c373df4715000bb05279ad16bdf4
Bjarki Bjorgvinsson <bjarki.bjorgvinsson98@gmail.com>
2025-09-11 14:16:17 +0100

    <feat> added prepare_input.py prepare_input.py contains functions to check the type of the input, and convert it to a dictionary if it is a fits file (with the correct extensions)

--- BlackWidowPipeline/prepare_input.py
+++ BlackWidowPipeline/prepare_input.py
@@ -1,13 +1,43 @@
-
+import numpy as np
 from astropy.io import fits
 
-def open_fits(path: str) -> fits.HDUList:
+def flatten_input(path: str, ext_dict = None) -> dict:
     '''
-    Open a FITS file and return the HDU list.
+    Open a FITS file, flatten its data, and return the flattened array. 
     path: str
         Path to the FITS file.
+    ext_dict: dict
+        Dictionary of emission lines and their corresponding extensions in the HDUList.
+        
+    returns:
+        data_dict: dict
+            Dictionary with emission lines as keys and flattened arrays as values.
     '''
-    with fits.open(path) as hdul:
-        return hdul
+    data_dict = {"Halpha":None, "Hbeta":None, "OII":None, "NII":None} # Dictionary for holding arrays
+    if  ext_dict is None:
+        ext_dict = {"Halpha":0, "Hbeta":1, "OII":2, "NII":3} # Defult extensions
+        
+    with fits.open(path) as hdul: # Open the FITS file
+        for line, ext in ext_dict.items(): # Loop over the emission lines
+            data_dict[line] = np.array((hdul[ext].data).flatten()) # Flatten and store in dictionary
     
-    
+    return data_dict
+
+def check_input(data = None):
+    '''
+    Check whether the input is a dictionary of numpy arrays or a path to a FITS file.
+    If it's a dictionary, validate its contents. If it's a path, process the FITS file with flatten_input.
+    data: dict or str
+        Either a dictionary with emission lines as keys and numpy arrays as values, or a path to a FITS file.
+    returns:
+        If input is a dictionary, returns the same dictionary after validation.
+        If input is a FITS file path, returns a dictionary with flattened arrays.
+    '''
+    if type(data) is dict:
+        assert all(isinstance(v, np.ndarray) for v in data.values()), "All values in the dictionary must be numpy arrays."
+        assert {"Halpha", "Hbeta", "OII", "NII"} <= data.keys(), "Dictionary must contain keys: 'Halpha', 'Hbeta', 'OIII', 'NII'."
+        return data
+    elif type(data) is str:
+        return flatten_input(data)
+    else:
+        raise ValueError("Input must be either a dictionary of numpy arrays or a path to a FITS file.")
--- tests/test_prepare_input.py
+++ tests/test_prepare_input.py
@@ -1,31 +1,45 @@
 import pytest
 import os
+import numpy as np
 from BlackWidowPipeline import prepare_input
 from astropy.io import fits
 
-def test__open_fits():
+def test__flatten_input():
     '''
-    Test opening a FITS file using astropy.io.fits
+    Test flattening result from input FITS file
     '''
-    # Create a sample FITS file for test
-    test_path = 'tests/sample.fits' 
-    test_hdu = fits.PrimaryHDU()
-    test_hdul = fits.HDUList([test_hdu])
-    test_hdul.writeto(test_path)
+    array = np.array([[1, 2, 3,],[4, 5, 6], [7, 8, 9]])
+    hdu = fits.PrimaryHDU(array)
+    hdu1 = fits.ImageHDU(array*2)
+    hdu2 = fits.ImageHDU(array*3)
+    hdu3 = fits.ImageHDU(array*4)
+    hdul = fits.HDUList([hdu, hdu1, hdu2, hdu3])
+    hdul.writeto('tests/sample.fits', overwrite=True)
     
-    file = prepare_input.open_fits(test_path)
-    assert isinstance(file, fits.HDUList) # Ensure file is HDUList
-    assert len(file) > 0  # Ensure the file has at least one HDU
+    data_dict = prepare_input.flatten_input('tests/sample.fits')
+    flattened = data_dict['Halpha']
+    assert isinstance(flattened, np.ndarray) # Ensure output is an array
+    assert np.array_equal(flattened, [1, 2, 3, 4, 5, 6, 7, 8, 9]) # Ensure correct flattening
+    assert np.array_equal(data_dict['Hbeta'], [2, 4, 6, 8, 10, 12, 14, 16, 18])
+    assert np.array_equal(data_dict['OIII'], [3, 6, 9, 12, 15, 18, 21, 24, 27])
+    assert np.array_equal(data_dict['NII'], [4, 8, 12, 16, 20, 24, 28, 32, 36])
+    # Cleanup
+    os.remove('tests/sample.fits')
     
-    # Deletes the testing file
-    os.remove(test_path)
+def test__check_input():
     
+    test_dict = {'Halpha': np.array([1,2,3]), 'Hbeta': np.array([4,5,6]), 'OIII': np.array([7,8,9]), 'NII': np.array([10,11,12])}
+    checked_dict = prepare_input.check_input(test_dict)
+    assert checked_dict == test_dict
     
-def test__flatten_array():
-    '''
-    Test flattening a given 2D array to 1D
-    '''
-    array_2d = [[1, 2, 3,],[4, 5, 6], [7, 8, 9]]
-    flattened = prepare_input.flatten_array(array_2d)
-    assert isinstance(flattened, list) # Ensure output is a list
-    assert flattened == [1, 2, 3, 4, 5, 6, 7, 8, 9] # Ensure correct flattening
+    array = np.array([[1, 2, 3,],[4, 5, 6], [7, 8, 9]])
+    hdu = fits.PrimaryHDU(array)
+    hdu1 = fits.ImageHDU(array*2)
+    hdu2 = fits.ImageHDU(array*3)
+    hdu3 = fits.ImageHDU(array*4)
+    hdul = fits.HDUList([hdu, hdu1, hdu2, hdu3])
+    hdul.writeto('tests/sample.fits', overwrite=True)
+    
+    checked_fits = prepare_input.check_input('tests/sample.fits')
+    assert isinstance(checked_fits, dict)
+    assert np.array_equal(checked_fits['Halpha'], array.flatten())
